9c46989e535b6a73e41063ef5cb0b5ede47f93d1
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-06-20 17:07:08 -0400

    changed app to class

--- src/App.js
+++ src/App.js
@@ -1,91 +1,96 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
+class App extends Component {
 
-      {/* NaV */}
-      <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Title</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Instructions</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Score</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Top Score</a>
-        </li>
-      </ul>
+  render() {
+    return (
 
-      {/* Jumbotron */}
-      <div class="jumbotron">
-        <h1 class="display-4">Clicky Game!</h1>
-        <p class="lead">Fuckfuckfuckfuckfuckfuckfuck.</p>
-      </div>
+      <div>
 
-      {/* Main Content */}
-      <div class="container">
+        {/* NaV */}
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Title</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Instructions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Score</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Top Score</a>
+          </li>
+        </ul>
 
-        <div class="row">
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
+        {/* Jumbotron */}
+        <div class="jumbotron">
+          <h1 class="display-4">Clicky Game!</h1>
+          <p class="lead">Fuckfuckfuckfuckfuckfuckfuck.</p>
         </div>
 
-        <div class="row">
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-          <div class="col-3">
-            <h1>I'm A Picture!</h1>
-          </div>
-        </div>
-
-      </div>
-
-      {/* footer */}
-      <footer class="footer mt-auto py-3">
+        {/* Main Content */}
         <div class="container">
-          <span class="text-muted">Footer</span>
+
+          <div class="row">
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+            <div class="col-3">
+              <h1>I'm A Picture!</h1>
+            </div>
+          </div>
+
         </div>
-      </footer>
+
+        {/* footer */}
+        <footer class="footer mt-auto py-3">
+          <div class="container">
+            <span class="text-muted">Footer</span>
+          </div>
+        </footer>
 
 
-    </div>
-  );
+      </div>
+    );
+  }
+
 }
 
 export default App;
