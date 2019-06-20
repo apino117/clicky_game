b48af7dd2334c71a934911f6cde5acc842891030
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-06-20 17:40:52 -0400

    attached click event to the pictures

--- src/components/PokeCard/index.js
+++ src/components/PokeCard/index.js
@@ -5,7 +5,7 @@ function PokeCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+        <img alt={props.name} src={props.image} onClick={function() { alert('click'); }}/>
       </div>
     </div>
   );
