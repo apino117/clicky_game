c5c34ae24281e21d78c224d34c437cda3f013f46
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-06-21 17:51:15 -0400

    implemented shuffle function into render

--- src/components/PokeField/index.js
+++ src/components/PokeField/index.js
@@ -3,6 +3,17 @@ import "./style.css";
 import pokemon from "../../pokemon.json";
 import PokeCard from "../PokeCard";
 
+
+function shuffleArray(array) {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
 class PokeField extends React.Component {
 
     // Add state of pokemon: pokemon
@@ -14,12 +25,18 @@ class PokeField extends React.Component {
 
         alert("click");
 
+
     }
 
+
+
     renderPokemon() {
+
+        const shuffledPokemon = shuffleArray(this.state.pokemon);
+
         return (
             <div class="row">
-                {this.state.pokemon.map(pokemon => (
+                {shuffledPokemon.map(pokemon => (
                     <PokeCard
                         id={pokemon.id}
                         key={pokemon.id}
@@ -42,3 +59,4 @@ class PokeField extends React.Component {
 }
 
 export default PokeField;
+
